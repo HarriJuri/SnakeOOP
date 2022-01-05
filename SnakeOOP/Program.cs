11be94acc136083cb95f7064ab7416f9a088442c
Opilane TTHK <opilane@tthk.ee>
2022-01-05 15:26:39 +0200

    first commit

---
+++ SnakeOOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SnakeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 10;
+            char symb = "*";
+
+            int x1 = 11;
+            int y1 = 10;
+
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(symb);
+
+            Console.ReadLine();
+        }
+    }
+}
